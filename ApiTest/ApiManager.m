bfc24228ac7f2a501604621ab159a598f09d0ed9
Stephen Printup <stephen.printup@gmail.com>
2015-10-28 16:10:51 -0700

    -made table show users

--- ApiTest/ApiManager.m
+++ ApiTest/ApiManager.m
@@ -159,7 +159,38 @@ NSString *SERVER_API_BASE_URL = @"http://localhost:5000";
 
 #pragma mark CHALLENGE #3 - with a partner or on your own
 - (void)fetchAllUserDataWithCompletion:(void (^)(NSArray<User *> *))completion failure:(void (^)(void))failure {
+    // start talking to the server
+    NSURLSession *urlSession = [NSURLSession sharedSession];
     
+    // make a request object - NSMutableURLRequest
+    NSString *urlString = [NSString stringWithFormat:@"http://104.236.231.254:5000/user?auth=%@", self.authToken];
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    
+    // populate the request with the information from documentation
+    request.HTTPMethod = @"GET";
+    
+    //actually pass the info to the server?; everything is ready, but we havn't left yet; dataTaskWithRequest actually sends to the server;
+    NSURLSessionDataTask *dataTask = [urlSession dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        if (error)
+        {
+            NSLog(@"There was an error with response %ld", (long) ((NSHTTPURLResponse *) response).statusCode);
+        }
+        else
+        {
+            NSLog(@"Success with response %ld", (long) ((NSHTTPURLResponse *) response).statusCode);
+            if (((NSHTTPURLResponse *)response).statusCode == 200) {
+                NSArray *arrayOfUsers = [User usersFromData:data];
+                completion(arrayOfUsers);
+            }
+            else
+            {
+                failure();
+            }
+        }
+    }];
+    
+    [dataTask resume];
 }
 
 #pragma mark CHALLENGE #4 - with a partner or on your own
